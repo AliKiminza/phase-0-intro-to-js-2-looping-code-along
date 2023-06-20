25c4804a8d9000fe2f3dbec344ea98c60a3b98b2
alikiminza <ali.kiminza@student.moringaschool.com>
2023-06-20 10:26:02 +0300

    final changes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names) {
+    const appreciationMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      appreciationMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return appreciationMessages;
+  }
+
+  console.log(writeCards(names));
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  countDown(8);
